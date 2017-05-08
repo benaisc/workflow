d2592a1efe2f56bb2c5bffa3a824b8974c803858
guru <charles-eric.benais-hugot@etu.umontpellier.fr>
2017-05-08 19:25:58 +0200

    main_usage

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,15 @@
 #include "LibParser.h"
 
+void usage(){
+	printf("Usage: %s <path_to_assemblyXML>", argv[0]);
+}
 int main(int argc, char* argv[]){
   STCMAssembly stcm;
 
+  if(argc < 2){
+	usage();
+	exit(-1);
+  }
   parser(argv[1], &stcm);
 
   std::cout << stcm << std::endl;
